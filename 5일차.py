475aa56cbc77d830dd198f1fd28943326df4a219
humpback2568 <jkh2568@daum.net>
2020-10-05 20:48:38 +0900

    First commit

---
+++ 5일차.py
@@ -0,0 +1,21 @@
+# for waiting_num in range(1,101):
+#     print("대기번호 : {0}". format(waiting_num))
+
+# for 곱 in range(1,10):
+#     print("=={}단==".format(곱))
+
+#     for K in range(1,10):
+#         print("{} * {} = {}".format(곱,K,곱 * K))
+
+# numbers= [3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3]
+
+# while 3 in numbers:
+#     numbers.remove(3)
+#      print(numbers)
+
+import time
+처음 = time.time()
+while 처음 + 5 >=time.time():
+    pass
+print("프로그램이 종료되었습니다.")
+
