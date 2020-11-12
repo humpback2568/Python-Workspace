c09651cb9b5050070a0b78401073e70105628f2b
humpback2568 <jkh2568@daum.net>
2020-11-12 23:22:02 +0900

    main

---
+++ 201112.py
@@ -0,0 +1,9 @@
+# import theater_module
+# theater_module.price(3) #3명이서 영화보러 가는 가격
+# theater_module.price_army(5)
+
+# import theater_module as mv
+# mv. price_morning(6)
+
+from theater_module import price, price as pc
+pc(6)
